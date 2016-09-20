c84cc8aa98bf66d1ef963a0b831b104115a3ebc2
Camilo Díaz Repka <camilojdiaz@gmail.com>
2016-09-20 00:17:01 -0400

    Add test database and `IF NOT EXISTS` clause

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,6 @@
--- as `root`
-CREATE DATABASE featuretrak CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- as mysql's `root`
+CREATE DATABASE IF NOT EXISTS featuretrak CHARACTER SET utf8 COLLATE utf8_general_ci;
 GRANT ALL PRIVILEGES ON featuretrak.* To 'enders'@'localhost' IDENTIFIED BY 'game';
+
+CREATE DATABASE IF NOT EXISTS featuretrak_test CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL PRIVILEGES ON featuretrak_test.* To 'setec'@'localhost' IDENTIFIED BY 'astronomy';
